53d530285ec0a717209b9c2215ee279f09205924
Victor <victor@Macbook.local>
2024-07-20 02:56:31 +0200

    Added cesar

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,24 @@
-let rec pgcd a b = match b with 0 -> a | _ -> pgcd b (a mod b);;
+let cesar str shift =
+  let shift = shift mod 26 in
+  let shift_char c =
+    if 'a' <= c && c <= 'z' then
+      char_of_int
+        (int_of_char 'a' + ((int_of_char c - int_of_char 'a' + shift) mod 26))
+    else if 'A' <= c && c <= 'Z' then
+      char_of_int
+        (int_of_char 'A' + ((int_of_char c - int_of_char 'A' + shift) mod 26))
+    else c (* Not a number *)
+  in
+  String.map shift_char str
 
-let d = pgcd 21 15 in
-print_int d
+let cesar_inv str shift = cesar str (-shift)
+
+let () =
+  let clear_str = "Hello, world" in
+  let shift = 3 in
+
+  let encr_str = cesar clear_str shift in
+  Printf.printf "Encrypted: %s\n" encr_str;
+
+  let decr_str = cesar_inv encr_str shift in
+  Printf.printf "Decrypted: %s\n" decr_str
